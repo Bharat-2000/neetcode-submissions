45a1a9002abc25f2e5ddd1da0087fab6e6bb2be7
Bharat Choudhary <60561607+Bharat-2000@users.noreply.github.com>
2026-05-16 12:22:15 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // for(int i = 0; i<nums.size(); i++){
+        //     for(int j = i+1; j<nums.size(); j++){
+        //         if(nums[i] + nums[j] == target) return {i, j};
+        //     }
+        // }
+        // return {};
+
+        unordered_map<int, int> mpp;
+
+        for(int i = 0; i<nums.size(); i++){
+            mpp[nums[i]] = i;
+        }
+
+        for(int i = 0; i<nums.size(); i++){
+            int diff = target - nums[i];
+
+            if(mpp.count(diff) && mpp[diff] != i){
+                return {i, mpp[diff]};
+            }
+        }
+        return {};
+
+    }
+};
+
+/*
+iteration -> O(n^2) Bruteforce
+*/
