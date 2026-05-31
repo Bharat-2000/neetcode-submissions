60e9a76d9688714c181d7cbde4339f6b79ed3ef9
Bharat Choudhary <60561607+Bharat-2000@users.noreply.github.com>
2026-05-31 16:02:19 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool isAlphanumeric(char c) {
+        // Check for digits 0-9
+        bool isDigit = (c - '0' >= 0) && (c - '0' <= 9);
+        
+        // Check for uppercase letters A-Z
+        bool isUpper = (c - 'A' >= 0) && (c - 'A' <= 25);
+        
+        // Check for lowercase letters a-z
+        bool isLower = (c - 'a' >= 0) && (c - 'a' <= 25);
+        
+        // Return true if it matches any of the categories
+        return isDigit || isUpper || isLower;
+    }   
+    bool isPalindrome(string s) {
+        // string resStr = "";
+        // for(int i = 0; i<s.length(); i++){
+        //     if(isAlphanumeric(s[i])) resStr += tolower(s[i]);
+        // }
+        // cout<<resStr<<endl;
+
+        int i = 0; int j = s.length()-1;
+        // while(i<j){
+        //     if(resStr[i] == resStr[j]){ i++; j--;}
+        //     else {return false;}
+        // }
+
+        while(i < j){
+            while (i < j && !isAlphanumeric(s[i])) i++;
+            while (i < j && !isAlphanumeric(s[j])) j--;
+
+            while (i < j && tolower(s[i]) != tolower(s[j])) return false;
+
+            i++; j--;
+        }
+        return true;
+    }
+};
+
+/* Approach - 1 (Brute Force):
+
+store the value into another string with only aplhanumberic values
+iterate from left and from right at the same time via pointers 
+            
+             j
+wasitacaroracatisaw
+     i  
+
+
+
+
+*/
