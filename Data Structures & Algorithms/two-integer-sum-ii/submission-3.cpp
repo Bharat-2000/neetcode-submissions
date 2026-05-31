202ccf00a6a7d5e5f9c3ac81960144da1ff008b7
Bharat Choudhary <60561607+Bharat-2000@users.noreply.github.com>
2026-05-31 16:11:23 +0530

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        for(int i = 0; i<numbers.size(); i++){
+            for(int j = i+1; j<numbers.size(); j++){
+                if(numbers[i] + numbers[j] == target) return {i+1, j+1};
+            }
+        }
+
+        return {};
+    }
+};
+
+
+/*
+Approach - 1:
+
+take two loops i and j and do a n^2 approach
+
+*/
